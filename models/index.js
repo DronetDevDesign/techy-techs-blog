dfb00f1ac9f8b7359646e37aa796f64e47f36996
Ron Dronet <ron@dronetdevdesign.com>
2022-08-27 09:26:14 -0500

    initial commit on this branch after creating the Post model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // Import each Model into index.js file:
 const User = require('./User');
+const Post = require('./Post');
 
 
 // Export each Model from index.js file:
-module.exports = { User };
+module.exports = { User, Post };
