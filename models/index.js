bdbeaee9095494fdb7b5d788368f9556f0942600
Ron Dronet <ron@dronetdevdesign.com>
2022-08-27 11:43:43 -0500

    added post routes and tested in insomnia - all passed

--- models/index.js
+++ models/index.js
@@ -2,6 +2,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
 // Export each Model from index.js file:
 module.exports = { User, Post };
