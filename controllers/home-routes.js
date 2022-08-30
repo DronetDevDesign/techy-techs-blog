5211ca9f130af108d4fd8e6f2c2f22d6a7467352
Ron Dronet <ron@dronetdevdesign.com>
2022-08-30 18:38:36 -0500

    troubleshooting

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,39 +3,40 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  console.log(req.session);
-  Post.findAll({
-    order: [['created_at', 'DESC']],
-    attributes: [
-      'id',
-      'title',
-      'created_at',
-      'content'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      // pass a single post object into the homepage template
-      res.render('homepage', { posts });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  // console.log(req.session);
+  // Post.findAll({
+  //   order: [['created_at', 'DESC']],
+  //   attributes: [
+  //     'id',
+  //     'title',
+  //     'created_at',
+  //     'content'
+  //   ],
+  //   include: [
+  //     {
+  //       model: Comment,
+  //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+  //       include: {
+  //         model: User,
+  //         attributes: ['username']
+  //       }
+  //     },
+  //     {
+  //       model: User,
+  //       attributes: ['username']
+  //     }
+  //   ]
+  // })
+  //   .then(dbPostData => {
+  //     const posts = dbPostData.map(post => post.get({ plain: true }));
+  //     // pass a single post object into the homepage template
+  //     res.render('homepage', { posts });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
+  res.json("testing")
 });
 
 // post detail page
