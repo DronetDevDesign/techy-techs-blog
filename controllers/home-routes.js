25273e692d4854d078c4674d048a5c2e57142f1f
Ron Dronet <ron@dronetdevdesign.com>
2022-08-30 18:52:06 -0500

    troubleshooting

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,80 +3,78 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  // console.log(req.session);
-  // Post.findAll({
-  //   order: [['created_at', 'DESC']],
-  //   attributes: [
-  //     'id',
-  //     'title',
-  //     'created_at',
-  //     'content'
-  //   ],
-  //   include: [
-  //     {
-  //       model: Comment,
-  //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-  //       include: {
-  //         model: User,
-  //         attributes: ['username']
-  //       }
-  //     },
-  //     {
-  //       model: User,
-  //       attributes: ['username']
-  //     }
-  //   ]
-  // })
-  //   .then(dbPostData => {
-  //     const posts = dbPostData.map(post => post.get({ plain: true }));
-  //     // pass a single post object into the homepage template
-  //     res.render('homepage', { posts });
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   });
-  res.json("testing")
+  console.log(req.session);
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass a single post object into the homepage template
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // post detail page
 router.get('/post/:id', (req, res) => {
-  // console.log(req.session);
-  // Post.findAll({
-  //   attributes: [
-  //     'id',
-  //     'title',
-  //     'content',
-  //     'created_at',
-  //   ],
-  //   include: [
-  //     {
-  //       model: Comment,
-  //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-  //       include: {
-  //         model: User,
-  //         attributes: ['username']
-  //       }
-  //     },
-  //     {
-  //       model: User,
-  //       attributes: ['username']
-  //     }
-  //   ],
-  //   where: {
-  //     id: req.params.id
-  //   }
-  // })
-  //   .then(dbPostData => {
-  //     const posts = dbPostData.map(post => post.get({ plain: true }));
-  //     // pass a single post object into the homepage template
-  //     res.render('post', posts[0]);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   });
-  res.json("testing")
+  console.log(req.session);
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass a single post object into the homepage template
+      res.render('post', posts[0]);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 
